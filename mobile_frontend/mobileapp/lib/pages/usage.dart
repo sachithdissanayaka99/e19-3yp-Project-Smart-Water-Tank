2d4d126f770df804e01a240140c2cc8e22ce8a5f
jagath99 <e19423@eng.pdn.ac.lk>
2024-01-07 20:33:26 +0530

    chart added to app

--- mobile_frontend/mobileapp/lib/pages/usage.dart
+++ mobile_frontend/mobileapp/lib/pages/usage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Usage extends StatefulWidget{
   @override
@@ -6,11 +7,21 @@ class Usage extends StatefulWidget{
 }
 
 class _usage extends State<Usage>{
+
+  List<waterData> data = [
+    waterData('Sunday', 200),
+    waterData('monday', 150),
+    waterData('tuesday', 80),
+    waterData('wendesday', 50),
+    waterData('thursday', 190),
+    waterData('friday', 110),
+    waterData('saturday', 100),
+  ];
+
   @override
   Widget build(BuildContext context){
     final screenSize = MediaQuery.of(context).size; // Define screenSize variable
     return Scaffold(
-      backgroundColor: Colors.blue[100],
       appBar:AppBar(
         title: SizedBox(
           height: screenSize.height * 0.08, // Set maximum height for the image
@@ -22,16 +33,44 @@ class _usage extends State<Usage>{
         //centerTitle: true,
         actions:[ 
           // ignore: prefer_const_constructors
-          IconButton(onPressed:(){Navigator.pushNamed(context, '/home');}, icon: Icon(Icons.arrow_drop_down_sharp)
-          ,
-          color: Colors.black,), ],
+          IconButton(
+            onPressed:(){Navigator.pushNamed(context, '/home');}, 
+            icon: Icon(Icons.arrow_drop_down_sharp),
+            color: Colors.black,
+          ),
+        ],
         backgroundColor: Colors.white,
-        
-
-      ) ,
-         body: Text('under construction'),
-      
-      
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              title: ChartTitle(text: 'Water Usage'),
+              legend: Legend(isVisible: true),
+              tooltipBehavior: TooltipBehavior(enable: true),
+              series: <CartesianSeries<dynamic, dynamic>>[
+                LineSeries<waterData, String>(
+                  dataSource: data, 
+                  xValueMapper: (waterData water,_)=> water.day, 
+                  yValueMapper: (waterData water,_)=>water.water,
+                  name:'Water Usage',
+                  dataLabelSettings: DataLabelSettings(isVisible: true),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
+class waterData {
+  final String day;
+  final double water;
+
+  waterData(this.day, this.water);
+}
+ 
